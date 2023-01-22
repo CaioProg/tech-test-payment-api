1c0d8b4f630581473208ac930f8e877b683745aa
CaioProg <caioprog18@gmail.com>
2023-01-22 17:26:35 -0300

    BuscaVendaPorId

--- Controllers/VendaController.cs
+++ Controllers/VendaController.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using tech_test_payment_api.Context;
+using tech_test_payment_api.Models;
 
 namespace tech_test_payment_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class VendaController
+    public class VendaController : ControllerBase
     {
-        
+        private readonly OrganizadorContext _context;
+
+        public VendaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscaVendaPorId(int vendaId) 
+        {
+            var venda = _context.Vendas.Find(vendaId);
+
+            if(venda != null)
+                return Ok(venda);
+
+            return NotFound();
+        }
     }
 }
