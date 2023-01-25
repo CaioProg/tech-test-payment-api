c226affda1bcd6b1a3459a4415ee700116fb6ae8
CaioProg <caioprog18@gmail.com>
2023-01-25 17:49:29 -0300

    Criando a lógica da edição do Status

--- Controllers/VendaController.cs
+++ Controllers/VendaController.cs
@@ -33,7 +33,7 @@ namespace tech_test_payment_api.Controllers
         [HttpPost]
         public IActionResult CriarVenda(Venda venda)
         {
-            if(venda.Status.ToString() != "Aguardando pagamento")
+            if(venda.Status != "Aguardando pagamento")
                 return BadRequest("Na criação da venda só é permitido o Status: Aguardando Pagamento");
             
             _context.Add(venda);
@@ -41,16 +41,55 @@ namespace tech_test_payment_api.Controllers
             return CreatedAtAction(nameof(ObterVendaPorId), new { ObterVendaPorId = venda.VendaId }, venda);    
         }
 
-        [HttpPut]
+        [HttpPatch]
         public IActionResult EditarVenda(Venda venda, int id)
         {
-             var vendaBanco = _context.Vendas.Find(id);
+            var vendaBanco = _context.Vendas.Find(id);
+            string verificacaoStatus = venda.Status;
+
+            switch (verificacaoStatus)
+            {
+                case "Pagamento aprovado":
+                    if(vendaBanco.Status == "Aguardando pagamento")
+                    {
+                        vendaBanco.Status = venda.Status;
+                        break;
+                    }
+                        return BadRequest($"Não é possível editar a venda pra esse Status! {vendaBanco.Status}");
+
+                case "Cancelada":
+                    if(vendaBanco.Status == "Aguardando pagamento" || vendaBanco.Status == "Pagamento aprovado")
+                    {
+                        vendaBanco.Status = venda.Status;
+                        break;
+                    }
+                        return BadRequest($"Não é possível editar a venda pra esse Status! {vendaBanco.Status}");
+
+                case "Enviado para Transportadora":
+                    if(vendaBanco.Status == "Pagamento aprovado")
+                    {
+                        vendaBanco.Status = venda.Status;
+                        break;
+                    }
+                        return BadRequest($"Não é possível editar a venda pra esse Status! {vendaBanco.Status}");
+
+                case "Entregue":
+                    if(vendaBanco.Status == "Enviado para Transportadora")
+                    {
+                        vendaBanco.Status = venda.Status;
+                        break;
+                    }                        
+                        return BadRequest($"Não é possível editar a venda pra esse Status! {vendaBanco.Status}");
+
+                default:
+                    return BadRequest($"Escolha um Status válido!");
+                    break;
+            }
 
             vendaBanco.ItensVendidos = venda.ItensVendidos;
             vendaBanco.Data = venda.Data;
             vendaBanco.ClienteID = venda.ClienteID;
             vendaBanco.VendedorId = venda.VendedorId;
-            vendaBanco.Status = venda.Status;
 
             _context.Vendas.Update(vendaBanco);
             _context.SaveChanges();
